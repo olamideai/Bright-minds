ed2ab2fc7c6fe92570c1dd44e624a88afbbf7be7
olamideai <abubakryibrahim880@gmail.com>
2026-02-25 13:29:14 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,19 +1,11 @@
 export const QUESTION_BANK = {
-  "PHY 101/107 (1)": [
+  "GST 112": [
     {
-      q: "The maximum range of a projectile occurs at:",
-      o: { A: "30°", B: "45°", C: "60°", D: "90°" },
-      a: "B"
-    },
-    {
-      q: "A car travelling at 30 m/s overcomes a frictional force of 600 N. The power developed is:",
-      o: { A: "12,000 W", B: "15,000 W", C: "18,000 W", D: "20,000 W" },
-      a: "C"
-    },
+      
     // ... Include all your 50 questions here
   ],
   
-  "PHY 101/107 (2)": [
+  "EDU 121": [
     // All 50 questions from set 2
   ]
   
@@ -21,22 +13,22 @@ export const QUESTION_BANK = {
 };
 
 export const SUBJECT_TIME = {
-  "PHY 101/107 (1)": 900,  // 15 minutes in seconds
-  "PHY 101/107 (2)": 900
+  "GST 112": 600,  // 15 minutes in seconds
+  "EDU 121": 600
   
 };
 
 export const SUBJECT_INFO = {
-  "PHY 101/107 (1)": {
-    title: "Physics 101/107 (Set 1)",
-    description: "Mechanics, Vectors & Basic Physics",
-    questions: 50,
-    time: "15 mins"
+  "GST 112": {
+    title: "GST 112",
+    description: "Nigeria Peoples and Culture ",
+    questions: 35,
+    time: "10 mins"
   },
-  "PHY 101/107 (2)": {
-    title: "Physics 101/107 (Set 2)",
-    description: "Waves, Motion & Forces",
-    questions: 50,
-    time: "15 mins"
+  "EDU 121": {
+    title: "EDU 121",
+    description: "Adult/Special Education and Development ",
+    questions: 35,
+    time: "10 mins"
   }
 };
